056fc73537fa420a0f6cd89cd13e0ce7eb670f5a
ADITYA VERMA <73814945+ADITYAVOFFICIAL@users.noreply.github.com>
2022-11-28 22:20:44 +0530

    Add files via upload

---
+++ Input Output/Input Output - Easy.py
@@ -0,0 +1,23 @@
+# DIVMOD.PY
+# a = int(input("Enter number-1: "))
+# b = int(input("Enter number-2: "))
+# div = a//b
+# qu = a % b
+# print(f"{a} // {b} = {div}\n{a} % {b} = {qu}")
+
+#
+# rep = int(input())
+# word = input()
+# print(word*rep)
+
+# arithmeticbit.py
+p = float(input())
+q = float(input())
+r = int(p*q)
+print(int(r))
+s = r & int(p)
+print(s)
+if s == r:
+    print("True")
+else:
+    print("False")
