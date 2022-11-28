7d9f8058f8c406119667fafa0366762cbe8970b7
ADITYA VERMA <73814945+ADITYAVOFFICIAL@users.noreply.github.com>
2022-11-28 22:37:23 +0530

    Update Input Output - Easy.py

--- Input Output/Input Output - Easy.py
+++ Input Output/Input Output - Easy.py
@@ -6,6 +6,7 @@ div = a//b
 qu = a % b
 print(f"{a} // {b} = {div}\n{a} % {b} = {qu}")
 
+# -----------------------------------------------------------
 
 # Samplepg.py
 
@@ -13,7 +14,7 @@ rep = int(input())
 word = input()
 print(word*rep)
 
-# arithmeticbit.py
+arithmeticbit.py
 p = float(input())
 q = float(input())
 r = int(p*q)
@@ -24,3 +25,56 @@ if s == r:
     print("True")
 else:
     print("False")
+
+# -----------------------------------------------------------
+
+# Areaofrect.py
+
+L = int(input("L:"))
+B = int(input("B:"))
+print(L*B)
+
+# -----------------------------------------------------------
+
+# Conversions.py
+
+a = int(input("number: "))
+print(complex(a))
+print(oct(a))
+
+# -----------------------------------------------------------
+
+# arithmeticbit.py
+
+p = int(input())
+q = int(input())
+r = p**q
+print(r)
+s = r ^ p
+print(s)
+if s > r:
+    print("True")
+else:
+    print("False")
+
+# -----------------------------------------------------------
+
+# formatting.py
+
+x = input("x:")
+y = input("y:")
+z = x+y
+print(f"x={x},y={y},z={z}")
+
+# -----------------------------------------------------------
+
+# decimal.py
+
+x = int(input("X:"))
+y = float(input("Y:"))
+print(f"    {x}")
+print('%.4f' % y)
+
+# -----------------------------------------------------------
+
+# END OF QUESTIONS 8/8
